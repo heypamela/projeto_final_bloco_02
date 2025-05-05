ac66f0f64656298c5f555d13049c1d08fae27730
Pamela <pamsilva.trabalho@hotmail.com>
2025-05-05 10:03:45 -0300

    Created category CRUD methods

---
+++ src/main/java/com/generation/projeto_final_bloco_02/model/Categoria.java
@@ -0,0 +1,50 @@
+package com.generation.projeto_final_bloco_02.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+
+	@NotBlank
+    @Size(min =  5, max = 200)
+    @NotNull(message = "A descrição é obrigatória")
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }    
+}
